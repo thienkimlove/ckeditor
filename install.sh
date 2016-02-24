94c3659ff4cc78df67e419daad62725496791d23
Quan Do <do.manh.quan@vsi-international.com>
2016-02-24 15:06:41 +0700

    fix

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-bower install && git clone git@github.com:sunhater/kcfinder.git && cp ck_config.js bower_components/ckeditor/config.js &&  chmod -R 777 kcfinder/upload
+echo "Install CkEditor via bower.."
+bower install
+echo "Done. Clone KCFinder.."
+git clone git@github.com:sunhater/kcfinder.git
+echo "Done. Copy custom config to ckEditor.."
+cp ck_config.js bower_components/ckeditor/config.js
+echo "Done. Chmod 777 upload path.."
+chmod -R 777 kcfinder/upload
+echo "Done"
